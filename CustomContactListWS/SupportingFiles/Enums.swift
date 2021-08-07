29ece8cba1963bcc292953f587961b2c3845bc41
byLLIPyT <byllipyt@icloud.com>
2021-08-07 13:11:56 +0300

    +searchBar

---
+++ CustomContactListWS/SupportingFiles/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  CustomContactListWS
+//
+//  Created by Александр Уткин on 07.08.2021.
+//
+
+import Foundation
+
+enum CellIdentifier {
+   static let weatherListCell = "WeatherListCell"
+   static let cellIdentifier = "Cell"
+   static let detailViewControllerIdentifier = "showDetail"
+}
+
---
+++ CustomContactListWS/Views/ContactCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactCell.swift
+//  CustomContactListWS
+//
+//  Created by Александр Уткин on 07.08.2021.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+        // Configure the view for the selected state
+    }
+
+}
